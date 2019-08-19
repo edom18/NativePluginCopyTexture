99fa4dc810117da6b672bc2ab1aab76978db403c
edom18 <edo.m18@gmail.com>
2019-08-19 09:00:28 +0900

    feat: Added build settings params.

--- Assets/Editor/XcodePostProcess.cs
+++ Assets/Editor/XcodePostProcess.cs
@@ -24,6 +24,8 @@ public class XcodePostProcess : MonoBehaviour
 
         // フレームワークを追加する
         proj.AddFrameworkToProject(target, "Photos.framework", false);
+        proj.AddBuildProperty(target, "SWIFT_VERSION", "4.0");
+        proj.AddBuildProperty(target, "SWIFT_OBJC_BRIDGING_HEADER", "$(SRCROOT)/Libraries/Plugins/iOS/Bridge-Header.h");
 
         // 反映させる
         File.WriteAllText(projPath, proj.WriteToString());
