2a3511ce2c84fd1a74061806aa7239b957275b05
edom18 <edo.m18@gmail.com>
2019-08-19 09:06:54 +0900

    feat: Added rotater for a demo.

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 0.1f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.one * _speed);
+    }
+}
